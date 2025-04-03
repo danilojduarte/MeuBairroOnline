5e85eeb993bdb6316d1f125b2f95fc61a0f769f7
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-04-03 15:27:00 -0300

    Criando screen login

--- meu-bairro-online/src/components/header/header.jsx
+++ meu-bairro-online/src/components/header/header.jsx
@@ -1,8 +1,11 @@
 import { Image, Text } from "react-native";
+import { styles } from "./header.style.js";
+import icons from "../../constants/icons.js";
+
 function Header() {
   return <>
-  <Image />
-  <Text></Text>
+  <Image style={styles.logo}/>
+  <Text>Acesse sua conta</Text>
   </>
 }
 
---
+++ meu-bairro-online/src/components/header/header.style.js
@@ -0,0 +1,9 @@
+import { COLORS, FONT_SIZE } from "../../constants/themes";
+
+export const styles = {
+  logo: {
+        width: 170,
+        height: 50,
+
+      }
+}
