3dbbaa3b3d600f55c323c8c6d117305a1d5102f1
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-04-09 14:52:24 -0300

    Ajustando function para screen login

--- meu-bairro-online/App.js
+++ meu-bairro-online/App.js
@@ -5,7 +5,7 @@ import Registro2 from "./src/screens/registro2/registro2.jsx";
 export default function App() {
 
   return (<>
-    <Registro2 />
+    <Login />
   </>
   );
 }
--- meu-bairro-online/src/screens/login/login.jsx
+++ meu-bairro-online/src/screens/login/login.jsx
@@ -3,9 +3,18 @@ import { styles } from "./login.style.js";
 import Header from "../../components/header/header.jsx";
 import TextBox from "../../components/textbox/textbox.jsx";
 import Button from "../../components/button/button.jsx";
+import { useState } from "react";
 
 
 function Login() {
+
+  const [email, setEmail] = useState("");
+
+  function ProcessarLogin() {
+    email = "12345...";
+    console.log("OK!");
+  }
+
   return <View style={styles.container}>
     <Header texto="Acesse sua conta"/>
 
@@ -19,7 +28,7 @@ function Login() {
       </View>
 
       <View style={styles.form}>
-        <Button texto="Acessar" />
+        <Button texto="Acessar" onPress={ProcessarLogin}/>
       </View>
 
     </View>
