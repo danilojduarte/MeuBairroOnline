64943d9ba05df00624806754d55d435a10f0816c
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-05-08 16:28:00 -0300

    Add novos icones

--- meu-bairro-online/src/constants/icons.js
+++ meu-bairro-online/src/constants/icons.js
@@ -5,8 +5,25 @@ import logo2 from "../assets/logo-app-copia.png";
 import cart from "../assets/cart.png";
 import favoritoFull from "../assets/favorito-full.png";
 import empty from "../assets/empty.png";
+import endereco from "../assets/endereco.png";
+import more from "../assets/more.png";
+import dados from "../assets/meus-dados.png";
+import logout from "../assets/logout.png";
 
-
-
-
-export default { back, remove, logo, cart, favoritoFull, logo2, empty };
+export default {
+  back,
+  remove,
+  logo,
+  cart,
+  favoritoFull,
+  logo2,
+  empty,
+  endereco,
+  more,
+  dados,
+  logout,
+  endereco,
+  more,
+  dados,
+  logout,
+};
