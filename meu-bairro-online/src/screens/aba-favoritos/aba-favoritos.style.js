691a7933cf3e4327e28fa4bae5c2d41c8896d550
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-06-18 05:05:53 -0300

    Ajustando projeto

--- meu-bairro-online/src/screens/aba-favoritos/aba-favoritos.style.js
+++ meu-bairro-online/src/screens/aba-favoritos/aba-favoritos.style.js
@@ -1,24 +1,23 @@
 import { COLORS, FONT_SIZE } from "../../constants/themes";
 
 export const styles = {
-  container: {
-    flex: 1,
-    backgroundColor: COLORS.white,
-    paddingLeft: 12,
-    paddingRight: 12,
-    
-  },
-  containerList: {
-    flexGrow: 1,
-  },
-  empty: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingBottom: 80,
-  },
-  emptyText: {
-    fontSize: FONT_SIZE.sm,
-    color: COLORS.dark_gray,
-  },
-};
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.white,
+        paddingLeft: 12,
+        paddingRight: 12
+    },
+    containerList: {
+        flexGrow: 1
+    },
+    empty: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        paddingBottom: 80
+    },
+    emptyText: {
+        fontSize: FONT_SIZE.sm,
+        color: COLORS.dark_gray
+    }
+}
