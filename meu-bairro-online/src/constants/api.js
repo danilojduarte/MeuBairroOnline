4f85e4bce86462cd47094e54c77447d8f57b44b7
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-06-20 00:05:03 -0300

    Problema de acesso api ajustado

--- meu-bairro-online/src/constants/api.js
+++ meu-bairro-online/src/constants/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "http://192.168.0.3:3001",
+  baseURL: "http://192.168.0.6:3001",
   timeout: 10000, // 10 seconds timeout
 });
 
